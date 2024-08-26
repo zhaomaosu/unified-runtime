da3d305a6735349bf269c035b56d64aadca40759
Zhao, Maosu <maosu.zhao@intel.com>
2024-08-25 23:10:17 -0700

    [DeviceSanitizer] Fix interceptor destruction order
    Adapters are released earlier than loader by SYCL Runtime, so we hold them in interceptor to prevent crash

--- source/loader/layers/sanitizer/asan_interceptor.hpp
+++ source/loader/layers/sanitizer/asan_interceptor.hpp
@@ -22,6 +22,7 @@
 #include <optional>
 #include <queue>
 #include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 namespace ur_sanitizer_layer {
@@ -45,9 +46,8 @@ struct DeviceInfo {
     std::queue<std::shared_ptr<AllocInfo>> Quarantine;
     size_t QuarantineSize = 0;
 
-    // TODO: re-enable retaining and releasing device handles in DeviceInfo
-    // constructor/destructor. See PR
-    // https://github.com/oneapi-src/unified-runtime/pull/1883
+    // Device handles are special and alive in the whole process lifetime,
+    // so we needn't retain&release here.
     explicit DeviceInfo(ur_device_handle_t Device) : Handle(Device) {}
 
     ur_result_t allocShadowMemory(ur_context_handle_t Context);
@@ -194,6 +194,16 @@ class SanitizerInterceptor {
     ur_result_t eraseMemBuffer(ur_mem_handle_t MemHandle);
     std::shared_ptr<MemBuffer> getMemBuffer(ur_mem_handle_t MemHandle);
 
+    ur_result_t holdAdapter(ur_adapter_handle_t Adapter) {
+        std::scoped_lock<ur_shared_mutex> Guard(m_AdaptersMutex);
+        if (m_Adapters.find(Adapter) != m_Adapters.end()) {
+            return UR_RESULT_SUCCESS;
+        }
+        UR_CALL(getContext()->urDdiTable.Global.pfnAdapterRetain(Adapter));
+        m_Adapters.insert(Adapter);
+        return UR_RESULT_SUCCESS;
+    }
+
     std::optional<AllocationIterator> findAllocInfoByAddress(uptr Address);
 
     std::shared_ptr<ContextInfo> getContextInfo(ur_context_handle_t Context) {
@@ -255,6 +265,9 @@ class SanitizerInterceptor {
 
     std::unique_ptr<Quarantine> m_Quarantine;
     logger::Logger &logger;
+
+    std::unordered_set<ur_adapter_handle_t> m_Adapters;
+    ur_shared_mutex m_AdaptersMutex;
 };
 
 } // namespace ur_sanitizer_layer
